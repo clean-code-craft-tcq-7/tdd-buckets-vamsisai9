cc6c3858b395f57f1a1499fceca38790a57fff32
vamsisai9 <vamsi.sai.work@gmail.com>
2023-02-10 03:58:57 +0530

    variable reading issue 2

--- bitToAmpConverter.py
+++ bitToAmpConverter.py
@@ -4,6 +4,6 @@ def convert_to_amps(reading):
     return round(10 * reading / 4094)
 
 
-def convert_to_current(a2d_reading):
-    current = (a2d_reading / 1023) * 30 - 15
+def convert_to_current(adc_reading):
+    current = (adc_reading / 1023) * 30 - 15
     return round(abs(current))
