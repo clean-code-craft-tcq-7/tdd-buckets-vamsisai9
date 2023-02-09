9d851a4aad26889f133286675d62e32470a342ad
vamsisai9 <vamsi.sai.work@gmail.com>
2023-02-10 03:44:43 +0530

    new sesnsors

---
+++ bitToAmpConverter.py
@@ -0,0 +1,9 @@
+def convert_to_amps(reading):
+    if reading == 4095:
+        return None
+    return round(10 * reading / 4094)
+
+
+def convert_to_current(a2d_reading):
+    current = (a2d_reading / 1023) * 30 - 15
+    return abs(current)
--- tdd_ranges.test.py
+++ tdd_ranges.test.py
@@ -1,5 +1,6 @@
 import unittest
 import tdd_range 
+import bitToAmpConverter
 
 class TestGetRanges(unittest.TestCase):
     def test_single_range(self):
@@ -16,6 +17,52 @@ class TestGetRanges(unittest.TestCase):
         samples = [3]
         expected = []
         self.assertEqual(tdd_range.get_ranges(sorted(samples)), expected)
+
+
+class TestCurrentSensor(unittest.TestCase):
+    def test_ignore_error_readings(self):
+        self.assertIsNone(bitToAmpConverter.convert_to_amps(4095))
+        
+    def test_convert_to_amps(self):
+        self.assertEqual(bitToAmpConverter.convert_to_amps(0), 0)
+        self.assertEqual(bitToAmpConverter.convert_to_amps(2047), 5)
+        self.assertEqual(bitToAmpConverter.convert_to_amps(4094), 10)
+        self.assertEqual(bitToAmpConverter.convert_to_amps(1146), 3)
+
+class TestAnotherTenSensor(unittest.TestCase):
+    def test_convert_to_current(self):
+        # Test case 1: ADC reading of 0
+        a2d_reading = 0
+        expected_current = 15
+        result = bitToAmpConverter.convert_to_current(a2d_reading)
+        assert result == expected_current, f"Expected {expected_current} but got {result}"
+
+        # Test case 2: ADC reading of 1023
+        a2d_reading = 1023
+        expected_current = 15
+        result = bitToAmpConverter.convert_to_current(a2d_reading)
+        assert result == expected_current, f"Expected {expected_current} but got {result}"
+
+        # Test case 3: ADC reading of 511
+        a2d_reading = 511
+        expected_current = 0
+        result = bitToAmpConverter.convert_to_current(a2d_reading)
+        assert result == expected_current, f"Expected {expected_current} but got {result}"
+
+        # Test case 4: ADC reading of 256
+        a2d_reading = 256
+        expected_current = 7.5
+        result = bitToAmpConverter.convert_to_current(a2d_reading)
+        assert result == expected_current, f"Expected {expected_current} but got {result}"
+
+        # Test case 5: ADC reading of 767
+        a2d_reading = 767
+        expected_current = 11.25
+        result = bitToAmpConverter.convert_to_current(a2d_reading)
+        assert result == expected_current, f"Expected {expected_current} but got {result}"
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
